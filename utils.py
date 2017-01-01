1aeecc66c0a7879f24eaf5841219419120c087bd
MechCoder <mks542@nyu.edu>
2017-01-01 05:35:25 +0530

    Rewrite

--- utils.py
+++ utils.py
@@ -184,3 +184,101 @@ def audio_amplitudes_gen(wavdir, lyr_dir=None, batch_size=32,
 
 def write_audio(batch, dest_path):
     write(dest_path, 16000, (batch.ravel() * 32768).astype(np.int16))
+
+
+def gen_audio_phonemes_pairs(wavdir=None, phdir=None, batch_size=32, num_steps=40,
+                             random_state=None, step_shift=0, wav_dim=200,
+                             path="train"):
+    """
+    Audio raw-amplitude batch generator.
+
+    Step length - Predicts raw-amplitude of the next time-step using the
+    given step.
+
+    num_steps - Number of time steps of each sample.
+    """
+    rng = np.random.RandomState(random_state)
+    n_samples = samples_per_epoch(wavdir, batch_size, num_steps, wav_dim)
+
+    if path == "train":
+        wavdir = "data/wavs/train"
+        limit_counter = 545
+    else:
+        limit_counter = 147
+        wavdir = "data/wavs/valid"
+
+    if path == "train":
+        phdir = "data/single_phonemes/train"
+    else:
+        phdir = "data/single_phonemes/val"
+    phoneme_to_id = build_phonemes_vocab()
+
+    wavfiles = os.listdir(wavdir)
+    n_songs = len(wavfiles)
+    xs = []
+    ys = []
+    startptr = 0
+    song_ind = 0
+    step_length = wav_dim / 16000.0
+    sample_size = int(wav_dim * num_steps)
+
+    curr_wav = wavfiles[song_ind % n_songs]
+    wavpath = os.path.join(wavdir, wavfiles[song_ind % n_songs])
+    lyrpath = os.path.join(phdir, curr_wav[:-4] + ".txt")
+
+    _, current_amps = read(wavpath)
+    current_amps = current_amps / 32768.0
+    curr_phonemes = open(lyrpath, "r").readlines()
+    phonemes_times = [float(c.strip().split(" ")[-1]) for c in curr_phonemes]
+    phonemes = [c.strip().split(" ")[0] for c in curr_phonemes]
+    start_time = 0.0
+    counter = 0
+
+    while True:
+        counter += 1
+        xs = []
+        ys = []
+        batch_phonemes = []
+        batch_ind = 0
+
+        while batch_ind < batch_size:
+            if startptr + sample_size + step_size <= len(current_amps):
+                x = np.reshape(
+                    current_amps[startptr: startptr + sample_size], (num_steps, -1))
+                y = np.reshape(
+                    current_amps[startptr + step_size : startptr + sample_size + step_size],
+                    (num_steps, -1))
+                xs.append(x)
+                ys.append(y)
+                startptr += sample_size
+                batch_ind += 1
+                step_phonemes = return_time_phonemes(
+                    phonemes, phonemes_times, start_time, num_steps, step_length,
+                    phoneme_to_id, step_shift)
+                batch_phonemes.append(step_phonemes)
+                start_time += num_steps * step_length
+            else:
+                song_ind += 1
+                curr_wav = wavfiles[song_ind % n_songs]
+                wavpath = os.path.join(wavdir, curr_wav)
+                lyrpath = os.path.join(phdir, curr_wav[:-4] + ".txt")
+                _, current_amps = read(wavpath)
+
+                startptr = 0
+                current_amps = current_amps / 32768.0
+                curr_phonemes = open(lyrpath, "r").readlines()
+                phonemes_times = [float(c.strip().split(" ")[-1]) for c in curr_phonemes]
+                phonemes = [c.strip().split(" ")[0] for c in curr_phonemes]
+                start_time = 0.0
+
+        batch_ind = 0
+        if counter % limit_counter == 0:
+            rng.shuffle(wavfiles)
+        xs = np.array(xs)
+        ys = np.array(ys)
+
+        if return_phonemes:
+            batch_phonemes = np.array(batch_phonemes)
+            yield ([xs, ys, batch_phonemes], ys)
+        else:
+            yield ([xs, ys], ys)
