009e28bd564dea92c72561977e768e921c7f8471
MechCoder <mks542@nyu.edu>
2017-01-02 11:59:48 +0530

    Add split words functionality

--- utils.py
+++ utils.py
@@ -4,8 +4,6 @@ import numpy as np
 from scipy.io.wavfile import read
 from scipy.io.wavfile import write
 
-from keras.preprocessing.sequence import pad_sequences
-
 
 def build_phonemes_vocab(ph_dir):
     list_of_files = os.listdir(ph_dir)
@@ -82,6 +80,26 @@ def samples_per_epoch(wavdir, batch_size=32, num_steps=40, wav_dim=200):
         n_samples += int(len(amps) / batch_dim)
         remainder += len(amps) % batch_dim
     return n_samples + int(remainder / batch_dim)
+
+
+def split_words(lyrics_dir, num_words=3):
+    txt_files = os.listdir(lyrics_dir)
+    for txt_file in txt_files:
+        lines = open(os.path.join(lyrics_dir, txt_file), "r")
+        lyrics = lines.readlines()
+        lines.close()
+
+        split_lines = open(os.path.join(lyrics_dir, txt_file[:-4]) + "_split.txt", "w")
+        words = [word for l in lyrics for word in l.strip().split(" ")]
+        for i in range(len(words) // num_words):
+            per_lines = [words[j + i * num_words]  for j in range(num_words)]
+            split_lines.write(" ".join(per_lines) + "\n")
+
+        startptr =  len(words) // num_words * num_words
+        rem = len(words) - startptr
+        per_lines = [words[j + startptr] for j in range(rem)]
+        split_lines.write(" ".join(per_lines) + "\n")
+        split_lines.close()
 
 
 def audio_amplitudes_gen(wavdir, lyr_dir=None, batch_size=32,
@@ -187,88 +205,3 @@ def audio_amplitudes_gen(wavdir, lyr_dir=None, batch_size=32,
 
 def write_audio(batch, dest_path):
     write(dest_path, 16000, (batch.ravel() * 32768).astype(np.int16))
-
-
-def gen_audio_phonemes_pairs(wavdir=None, phdir=None, batch_size=32, num_steps=40,
-                             random_state=None, step_shift=0, wav_dim=200):
-    """
-    Audio raw-amplitude batch generator.
-
-    Step length - Predicts raw-amplitude of the next time-step using the
-    given step.
-
-    num_steps - Number of time steps of each sample.
-    """
-    rng = np.random.RandomState(random_state)
-
-    phoneme_to_id = build_phonemes_vocab(phdir)
-    n_samples = samples_per_epoch(wavdir, batch_size, num_steps, wav_dim)
-    wavfiles = os.listdir(wavdir)
-    n_songs = len(wavfiles)
-    xs = []
-    ys = []
-    startptr = 0
-    song_ind = 0
-    step_length = wav_dim / 16000.0
-    sample_size = int(wav_dim * num_steps)
-
-    curr_wav = wavfiles[song_ind % n_songs]
-    wavpath = os.path.join(wavdir, wavfiles[song_ind % n_songs])
-    lyrpath = os.path.join(phdir, curr_wav[:-4] + ".txt")
-
-    _, current_amps = read(wavpath)
-    current_amps = current_amps / 32768.0
-    curr_phonemes = open(lyrpath, "r").readlines()
-    phonemes_times = [float(c.strip().split(" ")[-1]) for c in curr_phonemes]
-    phonemes = [c.strip().split(" ")[0] for c in curr_phonemes]
-    start_time = 0.0
-    counter = 0
-
-    while True:
-        counter += 1
-        xs = []
-        ys = []
-        batch_phonemes = []
-        batch_ind = 0
-
-        while batch_ind < batch_size:
-            if startptr + sample_size + wav_dim <= len(current_amps):
-                x = np.reshape(
-                    current_amps[startptr: startptr + sample_size], (num_steps, -1))
-                y = np.reshape(
-                    current_amps[startptr + wav_dim : startptr + sample_size + wav_dim],
-                    (num_steps, -1))
-                xs.append(x)
-                ys.append(y)
-                startptr += sample_size
-                batch_ind += 1
-                step_phonemes = return_time_phonemes(
-                    phonemes, phonemes_times, start_time, num_steps, step_length,
-                    phoneme_to_id, step_shift)
-                batch_phonemes.append(step_phonemes)
-                start_time += num_steps * step_length
-            else:
-                song_ind += 1
-                curr_wav = wavfiles[song_ind % n_songs]
-                wavpath = os.path.join(wavdir, curr_wav)
-                lyrpath = os.path.join(phdir, curr_wav[:-4] + ".txt")
-                _, current_amps = read(wavpath)
-
-                startptr = 0
-                current_amps = current_amps / 32768.0
-                curr_phonemes = open(lyrpath, "r").readlines()
-                phonemes_times = [float(c.strip().split(" ")[-1]) for c in curr_phonemes]
-                phonemes = [c.strip().split(" ")[0] for c in curr_phonemes]
-                start_time = 0.0
-
-        batch_ind = 0
-        if counter % n_samples == 0:
-            rng.shuffle(wavfiles)
-        xs = np.array(xs)
-        ys = np.array(ys)
-
-        if phdir:
-            batch_phonemes = np.array(batch_phonemes)
-            yield ([xs, ys, batch_phonemes], ys)
-        else:
-            yield ([xs, ys], ys)
